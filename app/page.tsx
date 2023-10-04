ff1cf51f5550617d0d59c981cc5645b3ce2394eb
bruno654 <67012261+bruno654@users.noreply.github.com>
2023-10-04 00:56:13 +0000

    fix; margem do cabeçalho

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return(
-  <h1 className="text-center"> ESCOLA TECNOLÓGICA</h1>
+  <h1 className="text-center mt-8"> ESCOLA TECNOLÓGICA</h1>
   )
 }
