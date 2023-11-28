90720c8fff59c0a00bb6632fc760f4d39a8cde2a
bruno654 <67012261+bruno654@users.noreply.github.com>
2023-11-28 23:20:52 +0000

    fix: denovo

--- app/admin/courses/new/page.tsx
+++ app/admin/courses/new/page.tsx
@@ -1,20 +1,37 @@
-export default function NewCourse(){
-    function saveCourse(){
+import UploadButton from "@/app/components/UploadButton";
+import { sql } from "@vercel/postgres";
+import { useSearchParams } from "next/navigation";
+
+export const revalidate =0
+
+export default function NewCourse({
+    searchParams,
+  }: {
+    searchParams?: {
+      url?: string;  
+    };
+  }){
+    
+    const urlImage = searchParams?.url || '';
+
+    async function saveCourse(formData: FormData){
         "use server"
+        const title = formData.get("title") as string;
+        const description = formData.get("description") as string;
+        await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${urlImage})`
         console.log("Acessou a função")
     }
-    return(
+    return (
         <div>
-            <h1 className="text-white text-center text-4xl"> Cadastrar Curso</h1>
+            <h1 className="text-white text-center text-4xl">Cadastrar Cursos</h1>
             <form>
-                <input type="text" name= "title"placeholder="Digite o titulo do curso" /> <br /><br />
-                <input type="text" name= "description"placeholder="Digite a descrição do curso" /><br /><br />
-                <input type="text" name= "url" placeholder="Digite a url da imagem" /> <br /><br />
-                <button formAction={saveCourse}className="text-white">Salvar </button><br />
-
-
+                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
+                <input type="text" name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
+                <br/>
+                <UploadButton />
+                <button formAction={saveCourse} className="text-white">Salvar</button>
             </form>
-
         </div>
+
     )
 }
